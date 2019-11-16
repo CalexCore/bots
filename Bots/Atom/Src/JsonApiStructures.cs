e969fbbaaf2c8704856be1dac5faceefaf2197f1
hooftly <hooftly@TINDEVM001.tf-dev.org>
2019-11-15 19:58:37 -0700

    Remove Coin Gecko

--- Bots/Atom/Src/JsonApiStructures.cs
+++ Bots/Atom/Src/JsonApiStructures.cs
@@ -68,68 +68,6 @@ namespace Atom
         //public double Low { get; set; }
     }
 
-    [JsonObject]
-    public class CoinGeckoInfo
-    {
-        [JsonProperty("market_cap_rank")]
-        public int MarketCapRank { get; set; }
-
-        [JsonProperty("coingecko_rank")]
-        public int CoinGeckoRank { get; set; }
-
-        [JsonProperty("coingecko_score")]
-        public double CoinGeckoScore { get; set; }
-
-        [JsonProperty("developer_score")]
-        public double DeveloperScore { get; set; }
-
-        [JsonProperty("community_score")]
-        public double CommunityScore { get; set; }
-
-        [JsonProperty("public_interest_score")]
-        public double PublicInterestScore { get; set; }
-
-        [JsonProperty("market_data")]
-        public CoinGeckoMarketData MarketData { get; set; }
-    }
-
-    [JsonObject]
-    public class CoinGeckoMarketData
-    {
-        [JsonProperty("current_price")]
-        public CoinGeckoPrice CurrentPrice { get; set; }
-
-        [JsonProperty("market_cap")]
-        public CoinGeckoPrice MarketCap { get; set; }
-
-        [JsonProperty("total_volume")]
-        public CoinGeckoPrice TotalVolume { get; set; }
-
-        [JsonProperty("high_24h")]
-        public CoinGeckoPrice High24h { get; set; }
-
-        [JsonProperty("low_24h")]
-        public CoinGeckoPrice Low24h { get; set; }
-
-        [JsonProperty("price_change_percentage_24h")]
-        public double PercentageChange { get; set; }
-
-        [JsonProperty("price_change_24h")]
-        public double PriceChange { get; set; }
-    }
-
-    [JsonObject]
-    public class CoinGeckoPrice
-    {
-        [JsonProperty("btc")]
-        public double BTC { get; set; }
-
-        [JsonProperty("usd")]
-        public double USD { get; set; }
-
-        [JsonProperty("aud")]
-        public double AUD { get; set; }
-    }
 
     [JsonObject]
     public class LinkData
