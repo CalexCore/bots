abe62b5150c9a3d8af85fe1d8aaf70b0f01cc78b
Angry Wasp <angrywasp@live.com>
2019-11-16 12:51:56 +1100

    Fix atomic unit conversion for 6 decimal places

--- Nerva.Bots/Src/Helpers/Conversions.cs
+++ Nerva.Bots/Src/Helpers/Conversions.cs
@@ -4,9 +4,9 @@ namespace Nerva.Bots.Helpers
 {
     public static class Conversions
     {
-        public static double FromAtomicUnits(this ulong i) => Math.Round((double)i / 1000000000000.0d, 4);
+        public static double FromAtomicUnits(this ulong i) => Math.Round((double)i / 1000000.0d, 4);
 
-        public static ulong ToAtomicUnits(this double i) => (ulong)(i * 1000000000000.0d);
+        public static ulong ToAtomicUnits(this double i) => (ulong)(i * 1000000.0d);
 
         public static string ToMention(this ulong id)
         {
