3f79e33461a5077a2786dc60da87841567cc024e
hooftly <mhooft@tf-dev.org>
2019-12-13 09:07:00 -0700

    Fix Sub/Integrated Addresses

--- Bots/Fusion/Src/AccountHelper.cs
+++ Bots/Fusion/Src/AccountHelper.cs
@@ -70,8 +70,8 @@ namespace Fusion
         public static bool ParseAddressFromMessage(SocketUserMessage msg, out string address)
         {
             address = null;
-            List<string> addresses = new Regex(@"(NV|NS)[a-zA-Z0-9]{95}").Matches(msg.Content).Select(x => x.Value).ToList();
-            addresses.AddRange(new Regex(@"(Ni)[a-zA-Z0-9]{107}").Matches(msg.Content).Select(x => x.Value).ToList());
+            List<string> addresses = new Regex(@"(amit|asub)[a-zA-Z0-9]{98}").Matches(msg.Content).Select(x => x.Value).ToList();
+            addresses.AddRange(new Regex(@"(aint)[a-zA-Z0-9]{109}").Matches(msg.Content).Select(x => x.Value).ToList());
 
             if (addresses.Count == 0)
                 return false;
